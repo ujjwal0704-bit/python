2b974482d109bf9ec0984b1c530c980e71168754
ujjwal0704-bit <ujjwal.krishna2204@gmail.com>
2026-03-31 00:05:01 +0530

    1st commit

---
+++ prime number.py
@@ -0,0 +1,13 @@
+print("program started")
+n = int(input("Enter a number: "))
+
+if n>1:
+    for i in range(2,int(n*0.5)+1):
+        if n % i == 0:
+            print(" not prime")
+            break
+    else:
+        print("prime")
+    
+else:
+    print("not prime")
